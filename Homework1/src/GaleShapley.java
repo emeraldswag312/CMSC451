9907f403d2b3fa01c0bf365838329ec8ca9b7121
emeraldswag312 <79111172+emeraldswag312@users.noreply.github.com>
2023-02-08 22:49:54 -0500

    Updated GaleShapley.java
    Added comments and changed var names

--- Homework1/src/GaleShapley.java
+++ Homework1/src/GaleShapley.java
@@ -6,34 +6,55 @@ public class GaleShapley {
 		
 	public static void galeShapleyAlgorithm(ArrayList<ArrayList<Integer>> employers, 
 			ArrayList<ArrayList<Integer>> candidates) { 
-		/* occ for occupied */
+		
+		/* Create empty ArrayList that hold each employer/candidate's current candidate/employer.
+		   0 is the default value where an employer/candidate does not have a current 
+		   candidate/employer. Any other number indicates the employer/candidate. Each (index + 1)
+		   specifies the current company (index 0 is company 1). */
 		ArrayList<Integer> occEmployers = new ArrayList<>(Arrays.asList(0,0,0,0,0,0,0));
 		ArrayList<Integer> occCandidates = new ArrayList<>(Arrays.asList(0,0,0,0,0,0,0));
 	
+		/* Iterate as long as an employer that does not have a candidate exists */ 
 		while (occEmployers.contains(0)) {
-			int freeCompany = occEmployers.indexOf(0) + 1;
-			int priorityCandidate = employers.get(freeCompany - 1).get(0);
-			employers.get(freeCompany - 1).remove(0);
+			/* Get the first employer between 1-7 that does not have a current candidate. 
+			   Retrieve its topmost candidate of a employer and then pop it from their preference
+			   list because it is no longer viable after processing it */
+			int freeEmployer = occEmployers.indexOf(0) + 1;
+			int priorityCandidate = employers.get(freeEmployer - 1).get(0);
+			employers.get(freeEmployer - 1).remove(0);
 			
 			if (occCandidates.get(priorityCandidate - 1) == 0) {
-				occCandidates.set(priorityCandidate - 1, freeCompany);
-				occEmployers.set(freeCompany - 1, priorityCandidate);
+				/* If their top candidate does not have an employer, set both to occupied 
+				   with each other */
+				occCandidates.set(priorityCandidate - 1, freeEmployer);
+				occEmployers.set(freeEmployer - 1, priorityCandidate);
 			} else {
-				int currCompany = occCandidates.get(priorityCandidate - 1);
-				int currCompanyIndex = 
-						candidates.get(priorityCandidate - 1).indexOf(currCompany);
-				int freeCompanyIndex = 
-						candidates.get(priorityCandidate - 1).indexOf(freeCompany);
-				if (currCompanyIndex < freeCompanyIndex) {
+				/* Otherwise, retrieve the current employer of the candidate and compare the
+				   two indices. The lower-numbered index indicating most preferred employer
+				   will be selected. */
+				int currEmployer = occCandidates.get(priorityCandidate - 1);
+				int currEmployerIndex = 
+						candidates.get(priorityCandidate - 1).indexOf(currEmployer);
+				int freeEmployerIndex = 
+						candidates.get(priorityCandidate - 1).indexOf(freeEmployer);
+				if (currEmployerIndex < freeEmployerIndex) {
+					/* The candidate's current employer's index is less than the free
+					   employer's, so they prefer the current employer over the free
+					   employer */
 					continue;
 				} else {
-					occEmployers.set(freeCompany - 1, priorityCandidate);
-					occEmployers.set(currCompany - 1, 0);
-					occCandidates.set(priorityCandidate - 1, freeCompany);
+					/* The candidate decides the free employer is better. Then, 
+					   the appropriate changes are made in the occupied lists, and 
+					   the current employer is set to 0 so that it can be recognized
+					   as a free employer later on */
+					occEmployers.set(freeEmployer - 1, priorityCandidate);
+					occEmployers.set(currEmployer - 1, 0);
+					occCandidates.set(priorityCandidate - 1, freeEmployer);
 				}
 			}
 		}
 		
+		/* Printing out the choices */
 		System.out.println(occEmployers);
 		System.out.println(occCandidates);
 	}
@@ -56,7 +77,7 @@ public class GaleShapley {
 		candidates.add(new ArrayList<>(Arrays.asList(1,7,5,2,4,3,6)));
 		candidates.add(new ArrayList<>(Arrays.asList(5,4,1,7,3,2,6)));
 		candidates.add(new ArrayList<>(Arrays.asList(7,6,3,4,5,1,2)));		
-		
+	
 		galeShapleyAlgorithm(new ArrayList<>(employers), candidates);
 	}
 }
